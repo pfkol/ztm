05c4256fbd770698f6e966a560a65891f0922b3d
Piotr Kolodziej <pfkol@poczta.onet.pl>
2020-05-13 09:41:37 +0200

    added function GetZipGTFS in clss ZTM

--- Program.cs
+++ Program.cs
@@ -18,6 +18,15 @@ namespace ztm
         string source = @"https://ckan.multimediagdansk.pl/dataset/c24aa637-3619-4dc2-a171-a23eec8f2172/resource/30e783e4-2bec-4a7d-bb22-ee3e3b26ca96/download/gtfsgoogle.zip";
         string destination = @"c:\Users\Piotrek\Documents\Projekty VSC\ztm\file.zip";
         string destinationEx = @"c:\Users\Piotrek\Documents\Projekty VSC\ztm\destination\";
+
+        public void GetZipGTFS() {
+             // Is file downloading yet?
+            if (webClient != null)
+            return;
+
+            webClient = new WebClient();
+            webClient.DownloadFile(source,destination);
+        } 
     }
     
     
