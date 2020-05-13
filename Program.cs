4a06f819c1bdacc36190f304b3c003b5fda9591c
Piotr Kolodziej <pfkol@poczta.onet.pl>
2020-05-13 09:17:45 +0200

    first commit with empty Main funkction

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+
+namespace ztm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    
+    
+   
+}
